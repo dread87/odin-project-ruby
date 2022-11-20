ff742eda55871479674205f1acc5d65cfff92676
Dread <dread97@proton.me>
2022-11-20 23:40:29 +0200

    Add 'Tree' class

--- ruby-balanced-binary-search-tree/balanced_binary_search_tree.rb
+++ ruby-balanced-binary-search-tree/balanced_binary_search_tree.rb
@@ -13,3 +13,77 @@ class Node
   end
 end
 
+class Tree
+  attr_accessor :root
+
+  def initialize(array)
+    @root = build_tree(array.uniq.sort)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+    return Node.new(array.first) if array.length == 1
+
+    mid = array.length / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array[..mid - 1])
+    root.right = build_tree(array[mid..])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(data, root = @root)
+    return Node.new(data) if root.nil?
+
+    if root.data == data
+      root
+    elsif root.data < data
+      root.right = insert(data, root.right)
+    else
+      root.left = insert(data, root.left)
+    end
+
+    root
+  end
+
+  def minValueNode(node)
+    return node if node.left.nil?
+
+    minValueNode(node.left)
+  end
+
+  def delete(data, root = @root)
+    return root if root.nil?
+
+    if data < root.data
+      root.left = delete(data, root.left)
+    elsif data > root.data
+      root.right = delete(data, root.right)
+    elsif root.left.nil?
+      temp = root.right
+      root = nil
+      return temp
+    elsif root.right.nil?
+      temp = root.left
+      root = nil
+      return temp
+    else
+      temp = minValueNode(root.right)
+      root.data = temp.data
+      root.right = delete(temp.data, root.right)
+    end
+    root
+  end
+
+  def find(data, root = @root)
+    return root if root.nil? || root.data == data
+
+    root.data < data ? find(data, root.right) : find(data, root.left)
+  end
+end
